2ad2b068b598fbfdeb0268627b4edf7e219977fd
cymula <amula@cytonn.com>
2023-01-20 09:03:06 +0300

    added uselayouteffect, useparams hooks

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,19 +1,30 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState, useLayoutEffect } from "react";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 import Navigation from "../components/Navigation";
 
 const Single = () => {
   const location = useLocation();
+  const params = useParams();
+  let navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize");
+    return () => {
+      window.removeEventListener("resize");
+    };
+  }, []);
 
   const id = location.state?.id;
   useEffect(() => {
-    console.log(id);
+    console.log(params);
     fetch(`https://dummyjson.com/posts/${id}`)
       .then((res) => res.json())
       .then((json) => {
         setData(json);
-      });
+      })
+      .catch((e) => navigate.push("/"));
   }, [id]);
+
   const [data, setData] = useState({});
   return (
     <div>
